b0e82e5c48623989cb749ad87110db2e05ffe8e3
Alexander P.J <64828269+AlexPJ1996@users.noreply.github.com>
2022-12-28 21:52:44 -0500

    Add files via upload

---
+++ WinForm VB.NET - Ejemplos.vb
@@ -0,0 +1,19 @@
+Public Class Ejemplos
+    
+	'Procedimiento para solo recibir numeros en un TextBox, mediante el evento KeyPress
+	Private Sub MyKeyPress(ByRef e As System.Windows.Forms.KeyPressEventArgs)
+		e.Handled = Not IsNumeric(e.KeyChar) And Not Char.IsControl(e.KeyChar)
+    End Sub
+	
+	'Procedimiento para mostrar datos de un DataGridView en Label/TextBox
+	Private Sub CellE()
+        [Label/TextBox] = [DataGridView].CurrentRow.Cells([Columna]).Value
+    End Sub
+	
+	'Procedimiento para mostrar texto de un Label/TextBox con formato moneda
+	'Configurar a un evento del tipo Click para evitar errores en ejecución
+	Sub Moneda()
+	    [Label/TextBox] = FormatCurrency(([Label/TextBox]), "$ 0")
+		[Label/TextBox] = Format([Label/TextBox], "$ 0")
+	End Sub
+End Class
